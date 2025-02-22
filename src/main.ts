0129d315405b044afe1f23564982c22f58ac648a
Natalya Babenko <ifelseiforelif@gmail.com>
2025-02-22 13:49:34 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT, () => {
+    Logger.log(`Server started. http://localhost:${PORT}`);
+  });
+}
+bootstrap();
